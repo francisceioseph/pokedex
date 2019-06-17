5e049aea2a6a57d39396e0aad5cd7f8aad8537ce
Francisco Souza <francisco.souza@ifactory.com>
2019-06-17 13:42:13 -0300

    Add empty screens

---
+++ lib/src/screens/pokedex.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Pokedex extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(),
+      ),
+    );
+  }
+}
